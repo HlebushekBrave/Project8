dfcddd38e4a9e6b2be0cc43d0f7f9da0bc53c16c
HlebushekBrave <73088409+HlebushekBrave@users.noreply.github.com>
2021-04-19 00:14:23 +0700

    Update HanoiTower.py

--- HanoiTower.py
+++ HanoiTower.py
@@ -1,9 +1,9 @@
 def HanoiTower(n, from_, to_, additional):
     if n == 1:
-        print("Переместите диск 1 со стержня", from_, "на стержень", to_)
+        print('Переместите диск 1 со стержня', from_, 'на стержень', to_)
         return
     HanoiTower(n - 1, from_, additional, to_)
-    print("Переместите диск", n, "со стержня", from_, "на стержень", to_)
+    print('Переместите диск', n, 'со стержня', from_, 'на стержень', to_)
     HanoiTower(n - 1, additional, to_, from_)
 
 
